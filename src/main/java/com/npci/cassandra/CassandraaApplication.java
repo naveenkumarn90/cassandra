aa10f602f2955b7138f3073746ab84b52e1911bf
naveenk <navku1102@gmail.com>
2022-03-07 15:50:03 -0800

    first commit

---
+++ src/main/java/com/npci/cassandra/CassandraaApplication.java
@@ -0,0 +1,13 @@
+package com.npci.cassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/npci/cassandra/CassandraaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npci.cassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
